1506905e0893e802a8f714254e0ec3db49cf4326
jonas <you@example.com>
2023-11-02 09:42:24 +0100

    penThickness made

--- src/App.java
+++ src/App.java
@@ -1,7 +1,8 @@
 import processing.core.PApplet;
 
 public class App extends PApplet {
-    int penColor = 255;  
+    int penColor = 255;
+    int penThickness = 1;
 
     public static void main(String[] args) {
         PApplet.main("App");
@@ -16,7 +17,8 @@ public class App extends PApplet {
     }
 
     public void draw() {
-        stroke(penColor);  
+        stroke(penColor);
+        strokeWeight(penThickness); 
         if (mousePressed) {
             line(mouseX, mouseY, pmouseX, pmouseY);
         }
@@ -28,13 +30,17 @@ public class App extends PApplet {
         } else if (key == 'q' || key == 'Q') {
             exit();
         } else if (key == 'y' || key == 'Y') {
-            penColor = color(255, 255, 0);  
+            penColor = color(255, 255, 0);
         } else if (key == 'r' || key == 'R') {
-            penColor = color(255, 0, 0);    
+            penColor = color(255, 0, 0);
         } else if (key == 'b' || key == 'B') {
-            penColor = color(0, 0, 255);    
-        } else if (key == 'g' || key == 'G'){
-            penColor = color(0,128,0);
+            penColor = color(0, 0, 255);
+        } else if (key == 'g' || key == 'G') {
+            penColor = color(0, 128, 0);
+        } else if (key == '+') {
+            penThickness += 1.0; 
+        } else if (key == '-') {
+            penThickness = max(1, penThickness - 1); 
         }
     }
 }
