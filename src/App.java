6432c4ab282e78f641b66a8471c0eecf5ed79aae
jonas <you@example.com>
2023-11-02 09:11:13 +0100

    my first commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import processing.core.PApplet;
 
 public class App extends PApplet {
+    int penColor = 255;  
 
     public static void main(String[] args) {
         PApplet.main("App");
@@ -15,9 +16,25 @@ public class App extends PApplet {
     }
 
     public void draw() {
-        stroke(255);
+        stroke(penColor);  
         if (mousePressed) {
             line(mouseX, mouseY, pmouseX, pmouseY);
+        }
+    }
+
+    public void keyPressed() {
+        if (key == 'c' || key == 'C') {
+            background(0);
+        } else if (key == 'q' || key == 'Q') {
+            exit();
+        } else if (key == 'y' || key == 'Y') {
+            penColor = color(255, 255, 0);  
+        } else if (key == 'r' || key == 'R') {
+            penColor = color(255, 0, 0);    
+        } else if (key == 'b' || key == 'B') {
+            penColor = color(0, 0, 255);    
+        } else if (key == 'g' || key == 'G'){
+            penColor = color(0,128,0);
         }
     }
 }
